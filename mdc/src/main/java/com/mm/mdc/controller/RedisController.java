7826f4f08058b83e43d5d765964379179a9c7b91
cc <crackedcd@qq.com>
2018-06-26 15:33:11 +0800

    fix configserver.

--- mdc/src/main/java/com/mm/mdc/controller/RedisController.java
+++ mdc/src/main/java/com/mm/mdc/controller/RedisController.java
@@ -1,6 +1,6 @@
 package com.mm.mdc.controller;
 
-import com.mm.mdc.feign.ConfigFeign;
+import com.mm.mdc.controller.config.MdcConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +14,10 @@ public class RedisController {
     private String serverPort;
 
     @Autowired
-    ConfigFeign configFeign;
+    MdcConfig mdcConfig;
 
     @RequestMapping("/hi")
     public String index(@RequestParam String name) {
-        return String.format("Hi %s, I am %s, get config: %s", name, serverPort, configFeign.getConfigEnv());
+        return String.format("Hi %s, I am %s, get config: %s", name, serverPort, mdcConfig.getConfigName());
     }
 }
